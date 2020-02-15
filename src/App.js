47100494fcc0ce43d782a5e7fcd709bd491d8196
Rodrigo Prado Fontes <fontesr2@gmail.com>
2020-02-15 15:28:33 -0300

    add: navigation dependencies

--- src/App.js
+++ src/App.js
@@ -1,13 +1,6 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React from 'react'
 import { SafeAreaView, StyleSheet, ScrollView, View, Text, StatusBar } from 'react-native'
+import { enableScreens } from 'react-native-screens'
 
 import {
   Header,
@@ -16,6 +9,8 @@ import {
   DebugInstructions,
   ReloadInstructions
 } from 'react-native/Libraries/NewAppScreen'
+
+enableScreens()
 
 const App: () => React$Node = () => {
   return (
